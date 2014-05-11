4d6020193d7912b1b7ee118cc2fd0ed2d688a889
kejenga <kejenga@gmail.com>
2014-05-11 23:27:14 +0100

    Final project using framework
    Added css transitions to banner and fixed float issue inherited from
    foundation's css; sorted some of the sizing issues for different-sized
    screens (found foundation's show-class feature); added social media
    footer plus round icons throughout; added pagination navigation.

---
+++ final project/using_fw/js/js.js
@@ -0,0 +1,21 @@
+window.onload = function() {
+
+	var button=document.getElementsByClassName("bannerbox")
+	var caption=document.getElementsByClassName("caption") 
+
+	button.onclick = function() {
+
+	}
+		
+
+
+
+	// var result = document.getElementById("result"),
+	// 	celsius = document.getElementById("celsius"),
+	// 	button = document.getElementById("submit");
+
+ //  	button.onclick = function() {
+ //  		var sum = celsius.value * 1.8 + 32;
+ //  		result.innerHTML = sum;
+	// }
+}
